cfa5f24d2917417aeea0134646bb107946f6b704
Горнов Евгений <gornovzena99@gmail.com>
2026-04-28 21:41:59 +0300

    fix

--- geg_logger/__init__.py
+++ geg_logger/__init__.py
@@ -1,5 +1,11 @@
-# my_logger/__init__.py
-from .logger import Logger
+# geg_logger/__init__.py
+from .logger import Logger, _Logger
 
 __version__ = "1.0.0"
-__all__ = ["Logger"]
+__all__ = ["Logger", "_Logger"]
+
+
+# Удобная функция для быстрого создания логгера
+def get_logger(name: str) -> Logger:
+    """Быстрое создание логгера"""
+    return Logger(name)
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 # setup.py
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name="my-logger",
+    name="geg-logger",  # Для pip установки (можно с дефисом)
     version="1.0.0",
     description="Переиспользуемый логгер для Python проектов",
     author="Your Name",
-    packages=find_packages(),
+    packages=find_packages(),  # Найдет geg_logger папку
+    package_dir={"": "."},  # Явно указываем корневую директорию
     python_requires=">=3.7",
     install_requires=[],
 )
